45ec049babe1f1af0e0b51227c6784f5f047cbb3
Temur662 <temursayfutdinov3@gmail.com>
2024-05-16 10:48:14 -0400

    Added Nativewind CSS

--- MAS-SI-APP/src/app/(tabs)/index.tsx
+++ MAS-SI-APP/src/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Platform, View } from 'react-native';
+import { Image, StyleSheet, Platform, View, Text } from 'react-native';
 
 import { HelloWave } from '../../components/HelloWave';
 import ParallaxScrollView from '../../components/ParallaxScrollView';
@@ -7,8 +7,9 @@ import { ThemedView } from '../../components/ThemedView';
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
+    <View style={styles.container} className='backgroundColor'>
       < Image source={require("../../../assets/images/massiLogo.png")} style={styles.massiLogo} />
+      <Text className='text-2xl font-bold'>Hello World</Text>
     </View>
   )
 }
---
+++ MAS-SI-APP/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/app/tabs/index.{js,jsx,ts,tsx}", "./src/app/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
