c636af6cab57e321c26f04f9c6dc5508887195e1
Abubeckr Elcharfa <abubeckr39@gmail.com>
2024-09-14 10:51:21 -0400

    TestFlight Changes

--- MAS-SI-APP/src/lib/supabase.ts
+++ MAS-SI-APP/src/lib/supabase.ts
@@ -1,7 +1,5 @@
 import { createClient } from "@supabase/supabase-js"
-import { useAuth } from "../providers/AuthProvider"
-const supabaseURL = process.env.EXPO_PUBLIC_SUPABASE_URL_LOCAL!
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_LOCAL!
+const supabaseURL = process.env.EXPO_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON!
 
 export const supabase = createClient(supabaseURL, supabaseAnonKey)
-const { session } = useAuth()
